e2d2cd6ef7004b3c788509681148e26766e6c5a9
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-11-11 23:44:41 +0300

    NGINX load balancer

---
+++ Load.Balancer.NGINX/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", (HttpContext context) =>
+{
+    var client_ip = context.Request.Headers["X-Forwarded-For"].ToString();
+
+    return args[0] + " - " + client_ip;
+});
+
+app.Run();
